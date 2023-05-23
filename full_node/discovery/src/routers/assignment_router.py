01d83a3e23e936d5f5c86edc9d8e05af649ae8ba
jeryongchan <jeryong96@gmail.com>
2023-05-23 09:40:44 +0800

    updated key verification

--- full_node/discovery/src/routers/assignment_router.py
+++ full_node/discovery/src/routers/assignment_router.py
@@ -19,6 +19,7 @@ async def assign_host(
     didModel: DIDModel = Body(..., description="DID of the client"),
     assignment_service: AssignmentService = Depends(get_assignment_service),
 ):
+    print("didModel", didModel)
     did = didModel.did
     queue = assignment_service.assign(did)
     return {"queue": queue}
--- full_node/discovery/src/routers/login_router.py
+++ full_node/discovery/src/routers/login_router.py
@@ -24,10 +24,26 @@ async def verify_response(
     login_service: LoginService = Depends(get_login_service)
 ):
     data = await request.json()
-    did = data['did']
-    signedChallenge = data['signedChallenge']
-    challenge = data['challenge']
-    print("signedChallenge", signedChallenge)
-    is_authenticated = login_service.verify_response(did, signedChallenge, challenge)
+    message = data['message']
+    publicKey = data['publicKey']
+    signature = data['signature']
+    # print("signedChallenge", signedChallenge)
+    # print(message)
+    is_authenticated = login_service.verify_response(message, signature, publicKey)
     print("is_authenticated", is_authenticated)
     return is_authenticated
+
+
+# @login_router.post("/verify_response/")
+# async def verify_response(
+#     request: Request, 
+#     login_service: LoginService = Depends(get_login_service)
+# ):
+#     data = await request.json()
+#     did = data['did']
+#     signedChallenge = data['signedChallenge']
+#     challenge = data['challenge']
+#     print("signedChallenge", signedChallenge)
+#     is_authenticated = login_service.verify_response(did, signedChallenge, challenge)
+#     print("is_authenticated", is_authenticated)
+#     return is_authenticated
