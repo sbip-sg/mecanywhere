0106880da43f60fd9a109baf647340cff64ee367
Lim Junxue <junxue1@hotmail.com>
2023-10-04 11:54:13 +0800

    Fix ganache and DID proxied requests
    Proxy to ganache was not exposed on the container.
    DID swagger ui was requesting to the internal url
    instead of proxy url

--- did/contract/truffle-config.js
+++ did/contract/truffle-config.js
@@ -77,7 +77,7 @@ module.exports = {
         },
         docker_sbip: {
             host: "sbip-g2.d2.comp.nus.edu.sg",
-            port: 11001,
+            port: 11011,
             network_id: "*",
             // production: true // - set to true if you would like to force a dry run to be performed every time you migrate using this network (default: false)
         },
