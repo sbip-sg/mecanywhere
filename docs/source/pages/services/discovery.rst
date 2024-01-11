81e611a0077329b56f131732ecf19f5172c1cb53
Lim Junxue <60027105+LimJunxue@users.noreply.github.com>
2024-01-11 18:38:11 +0800

    Gpu support (#66)
    * Update deprecated functions in docs
    
    * Add gpu message fields
    
    * Update docs

---
+++ docs/source/pages/services/discovery.rst
@@ -0,0 +1,7 @@
+Discovery
+=========
+
+Flow
+----
+
+3rd party app (use library): SDK (socket connection): Desktop App (http request): Discovery Service (AMQP): RabbitMQ (AMQP): Desktop RMQ queue (http request): Task Executor: Docker 
--- load_po.bat
+++ load_po.bat
@@ -10,5 +10,5 @@ call truffle migrate --network development
 @REM call truffle migrate --network docker_sbip
 cd ..\..
 
-python load_po.py localhost:8080 localhost:9090
-@REM python load_po.py sbip-g2.d2.comp.nus.edu.sg:11000/did-verifier sbip-g2.d2.comp.nus.edu.sg:11000/did-issuer
+python util/load_po.py localhost:8080 localhost:9090
+@REM python util/load_po.py sbip-g2.d2.comp.nus.edu.sg:11000/did-verifier sbip-g2.d2.comp.nus.edu.sg:11000/did-issuer
