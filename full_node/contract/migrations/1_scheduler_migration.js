8ffcac4bbd3c36a10be395526fd163788d34ddf6
Lim Junxue <junxue1@hotmail.com>
2024-02-26 11:33:24 +0800

    Remove extra contracts

--- full_node/contract/migrations/1_scheduler_migration.js
+++
@@ -1,5 +0,0 @@
-const Contract = artifacts.require("SchedulerContract");
-
-module.exports = function (deployer) {
-    deployer.deploy(Contract);
-};
--- full_node/contract/migrations/2_tower_migration.js
+++
@@ -1,5 +0,0 @@
-const Contract = artifacts.require("TowerContract");
-
-module.exports = function (deployer) {
-    deployer.deploy(Contract);
-};
