70c69cd2afda878a6cb901e060a31c6099e1e1b2
Lim Junxue <junxue1@hotmail.com>
2024-07-10 13:35:00 +0800

    Update docs

--- example-tasks/python-template/src/app.py
+++ example-tasks/python-template/src/app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def entry_point():
     input = request.json
+    print("Request:", request)
+    print("Input:", input)
 
     return json.dumps(input)
 
